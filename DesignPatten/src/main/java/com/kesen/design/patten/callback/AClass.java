91280a10c95bbbfe2973c26364a5c42199795f67
kesenHuang <huangkai_uestc@163.com>
2020-05-11 21:18:40 +0800

    回调

---
+++ DesignPatten/src/main/java/com/kesen/design/patten/callback/AClass.java
@@ -0,0 +1,9 @@
+package com.kesen.design.patten.callback;
+
+/**
+ * @Auther: kesen
+ * @Date: 2020/5/11 20:42
+ * @Description:
+ **/
+public class AClass {
+}
---
+++ DesignPatten/src/main/java/com/kesen/design/patten/callback/ICallback.java
@@ -0,0 +1,4 @@
+package com.kesen.design.patten.callback;
+
+public interface ICallback {
+}
